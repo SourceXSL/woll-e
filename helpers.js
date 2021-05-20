ad79b4e2b03d8cef398fe7935000d38bd27c347f
SourceXSL <84072118+SourceXSL@users.noreply.github.com>
2021-05-20 09:58:39 -0400

    Add files via upload

---
+++ helpers.js
@@ -0,0 +1,24 @@
+/*
+# Exclusively from SL-Android project 
+# Do not use this fore any commercial thing
+# If you abuse thais bot we wil kick you from bot 
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved ©Asanga_Udara
+*/
+
+function successfullMessage(msg) {
+    return "✅ *AleXa Done*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "🛑 *AleXa Faild t.me/Asanga_Udara*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "⏺️ *AleXa*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
